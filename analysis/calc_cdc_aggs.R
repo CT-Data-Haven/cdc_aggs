7ac717d3a949c3300bdbff18437ec97e5a5d05c3
camille-s <camille.seaberry@gmail.com>
2022-04-18 18:14:33 -0400

    Update with miscellaneous new regions

--- analysis/calc_cdc_aggs.R
+++ analysis/calc_cdc_aggs.R
@@ -4,24 +4,35 @@ library(cwi)
 ############# META
 # hosp_regs <- readRDS("utils/hospital_areas_list.rds")
 # includes hospitals
-extra_regs <- readRDS("utils/misc_regions_list.rds")
+# extra_regs <- readRDS("utils/misc_regions_list.rds")
+reg_puma_list <- readRDS("utils/reg_puma_list.rds")
 meta <- read_csv("utils/cdc_indicators.txt")
 nhood_wts <- lst(new_haven_tracts19, bridgeport_tracts19, hartford_tracts19, stamford_tracts19) %>%
   set_names(str_remove, "_tracts.+") %>%
   set_names(camiller::clean_titles, cap_all = TRUE) %>%
   bind_rows(.id = "city")
-tract2reg <- c(cwi::regions[c("Greater New Haven", "Greater Hartford", 
-                              str_subset(names(cwi::regions), "^[A-Z][\\w\\s]+County$"))], 
-               extra_regs) %>%
-  enframe(value = "town") %>%
-  unnest(town) %>%
-  inner_join(tract2town, by = "town") %>%
-  select(-town)
-tract2puma <- cwi::xwalk %>%
-  filter(!str_detect(puma, "^[A-Z][\\w\\s]+ County$")) %>%
-  distinct(puma_fips, tract)
 
-pops15 <- tidycensus::get_acs("tract", table = "B01003", year = 2015, state = "09") %>%
+tract2reg <- bind_rows(
+  enframe(reg_puma_list, value = "town") %>%
+    unnest(town),
+  distinct(cwi::xwalk, county, town) %>%
+    rename(name = county)
+) %>%
+  inner_join(tract2town, by = "town") %>%
+  distinct(name, tract)
+
+# tract2reg <- c(cwi::regions[c("Greater New Haven", "Greater Hartford", 
+#                               str_subset(names(cwi::regions), "^[A-Z][\\w\\s]+County$"))], 
+#                extra_regs) %>%
+#   enframe(value = "town") %>%
+#   unnest(town) %>%
+#   inner_join(tract2town, by = "town") %>%
+#   select(-town)
+# tract2puma <- cwi::xwalk %>%
+#   filter(!str_detect(puma, "^[A-Z][\\w\\s]+ County$")) %>%
+#   distinct(puma_fips, tract)
+
+pops15 <- tidycensus::get_acs("tract", table = "B01003", year = 2015, state = "09", cache_table = TRUE) %>%
   janitor::clean_names() %>%
   select(geoid, pop = estimate)
 
@@ -42,9 +53,9 @@ pl_lvls[["state"]] <- places %>%
   mutate(name = "Connecticut")
 pl_lvls[["regions"]] <- places %>%
   inner_join(tract2reg, by = c("geoid" = "tract"))
-pl_lvls[["pumas"]] <- places %>%
-  inner_join(tract2puma, by = c("geoid" = "tract")) %>%
-  rename(name = puma_fips)
+# pl_lvls[["pumas"]] <- places %>%
+#   inner_join(tract2puma, by = c("geoid" = "tract")) %>%
+#   rename(name = puma_fips)
 pl_lvls[["towns"]] <- places %>%
   left_join(tract2town, by = c("geoid" = "tract")) %>%
   rename(name = town)
@@ -54,7 +65,7 @@ pl_lvls[["neighborhoods"]] <- places %>%
 pl_lvls[["tracts"]] <- places %>%
   rename(name = geoid)
 places_df <- bind_rows(pl_lvls, .id = "level") %>%
-  mutate(level = as_factor(level),
+  mutate(level = as_factor(ifelse(grepl("^\\d{7}$", name), "puma", level)),
          year = as.character(year)) %>%
   group_by(topic, question, level, year, city, town, name) %>%
   summarise(value = weighted.mean(value, pop)) %>%
@@ -73,9 +84,9 @@ life_lvls[["state"]] <- life_exp %>%
   mutate(name = "Connecticut")
 life_lvls[["regions"]] <- life_exp %>%
   inner_join(tract2reg, by = "tract")
-life_lvls[["pumas"]] <- life_exp %>%
-  inner_join(tract2puma, by = "tract") %>%
-  rename(name = puma_fips)
+# life_lvls[["pumas"]] <- life_exp %>%
+#   inner_join(tract2puma, by = "tract") %>%
+#   rename(name = puma_fips)
 life_lvls[["towns"]] <- life_exp %>%
   left_join(tract2town, by = "tract") %>%
   rename(name = town)
@@ -85,7 +96,7 @@ life_lvls[["neighborhoods"]] <- life_exp %>%
 life_lvls[["tracts"]] <- life_exp %>%
   rename(name = tract)
 life_df <- bind_rows(life_lvls, .id = "level") %>%
-  mutate(level = as_factor(level)) %>%
+  mutate(level = as_factor(ifelse(grepl("^\\d{7}$", name), "puma", level))) %>%
   group_by(topic = "life_expectancy", question, level, year, city, town, name) %>%
   summarise(value = weighted.mean(value, pop)) %>%
   ungroup() %>%
